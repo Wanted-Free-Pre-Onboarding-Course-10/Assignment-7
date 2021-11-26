b15819799175bb4a6bbdcd55c0d10889c869651c
ji-park <qkrskdjf@naver.com>
2021-11-26 16:30:40 +0900

    Feat : 에러처리 및 회원가입

---
+++ src/service/user.service.ts
@@ -0,0 +1,21 @@
+import { getConnection, QueryRunner, Repository } from "typeorm";
+import { User } from "../entity/user";
+
+export class UserService {
+    private userRepository: Repository<User>;
+
+    constructor() {
+        this.userRepository = getConnection().getRepository(User);
+    }
+
+    async createUser(createUserInfo) {
+        const { email } = createUserInfo;
+        const user = await this.userRepository
+            .findOne({ where: { email: email } });
+        if (user) {
+            return { exUser: user, newUser: undefined };
+        }
+        const newUser = await this.userRepository.save(createUserInfo);
+        return { exUser: undefined, newUser: newUser };
+    }
+}
